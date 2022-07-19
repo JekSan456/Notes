5d0d7afb410243d3e0da7473d2e44d69fe148a64
Jek <evgeniykondrenko@gmail.com>
2022-07-19 22:42:49 +0300

    Реализована вёрстка сплэш экрана

---
+++ app/src/main/java/com/jek/notes/SpashScreenActivity.kt
@@ -0,0 +1,14 @@
+package com.jek.notes
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SpashScreenActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_activity)
+    }
+
+}
